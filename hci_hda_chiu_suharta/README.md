e0d5dd266259cf48f2ca0b5a34c2e2809c38dc37
Patrick Chiu <patrick.a.chiu@stud.h-da.de>
2024-05-17 13:39:30 +0200

    First Commit

---
+++ hci_hda_chiu_suharta/README.md
@@ -0,0 +1,3 @@
+# hci_hda_chiu_suharta
+
+A new Flutter project.
---
+++ hci_hda_chiu_suharta/android/app/src/main/kotlin/com/example/hci_hda_chiu_suharta/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hci_hda_chiu_suharta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
