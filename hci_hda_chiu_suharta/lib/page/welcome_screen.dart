187decaa21bf3a74f6b8f4d61a72230bd02cc776
Patrick Chiu <patrick.a.chiu@stud.h-da.de>
2024-05-30 17:02:30 +0200

    Welcome screen done

---
+++ hci_hda_chiu_suharta/lib/page/welcome_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hci_hda_chiu_suharta/page/sign_in_screen.dart';
+import 'package:hci_hda_chiu_suharta/page/sign_up_page.dart';
+import 'package:hci_hda_chiu_suharta/theme/theme.dart';
+import 'package:hci_hda_chiu_suharta/widgets/custom_scaffold.dart';
+import 'package:hci_hda_chiu_suharta/widgets/welcome_button.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      child: Column(
+        children: [
+          Flexible(
+              flex: 8,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 0,
+                  horizontal: 40.0,
+                ),
+                child: Center(
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: const TextSpan(children: [
+                      TextSpan(
+                          text: 'Welcome to FahrrAzt!\n',
+                          style: TextStyle(
+                            fontSize: 45.0,
+                            fontWeight: FontWeight.w600,
+                          )),
+                    ]),
+                  ),
+                ),
+              )),
+          Flexible(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Sign In',
+                      onTap: SignInScreen(),
+                      color: Colors.transparent,
+                      textColor: Colors.black,
+                    ),
+                  ),
+                  Expanded(
+                    child: WelcomeButton(
+                      buttonText: 'Sign Up',
+                      onTap: const SignUpScreen(),
+                      color: Colors.white,
+                      textColor: lightColorScheme.primary,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
