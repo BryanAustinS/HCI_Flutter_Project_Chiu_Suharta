19edd7cfd14673a880d7d75a4a2556b12aba2cf3
Patrick Chiu <patrickchiu0902@gmail.com>
2024-05-31 01:01:00 +0200

    signup page on progress

--- hci_hda_chiu_suharta/lib/page/sign_up_page.dart
+++ hci_hda_chiu_suharta/lib/page/sign_up_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hci_hda_chiu_suharta/theme/theme.dart';
 import 'package:hci_hda_chiu_suharta/widgets/custom_scaffold.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -9,10 +10,138 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  final _formSignUpKey = GlobalKey<FormState>();
+  bool agreePersonalData = true;
   @override
   Widget build(BuildContext context) {
-    return const CustomScaffold(
-      child: Text('Sign Up'),
+    return CustomScaffold(
+      child: Column(children: [
+        const Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 10,
+          ),
+        ),
+        Expanded(
+          flex: 7,
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(25.0, 50.0, 25.0, 20.0),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40.0),
+                topRight: Radius.circular(40.0),
+              ),
+            ),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formSignUpKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Get Started',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.w900,
+                        color: lightColorScheme.primary,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40.0,
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Full name';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        label: const Text('Full Name'),
+                        hintText: 'Enter Full Name',
+                        hintStyle: const TextStyle(
+                          color: Colors.black26,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.black12,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25.0,
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Email';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        label: const Text('Email'),
+                        hintText: 'Enter Email',
+                        hintStyle: const TextStyle(
+                          color: Colors.black26,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.black12,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.black12,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25.0,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      obscuringCharacter: '*',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter password';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        label: const Text(
+                          'Password',
+                        ),
+                        hintText: 'Enter Password',
+                        hintStyle: const TextStyle(
+                          color: Colors.black26,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.black12,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.black12,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
