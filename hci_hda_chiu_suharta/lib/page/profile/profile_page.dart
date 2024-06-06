7776682aab2168f1753a05d6fe995645458f51fa
Patrick Chiu <patrickchiu0902@gmail.com>
2024-06-06 22:11:23 +0200

    profile_page.dart update and add ionicons package

--- hci_hda_chiu_suharta/lib/page/profile/profile_page.dart
+++ hci_hda_chiu_suharta/lib/page/profile/profile_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:ionicons/ionicons.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){},
+          icon: const Icon(Ionicons.chevron_back_outline),
+        ),
+        leadingWidth: 100,
+      ),
 
     );
   }
