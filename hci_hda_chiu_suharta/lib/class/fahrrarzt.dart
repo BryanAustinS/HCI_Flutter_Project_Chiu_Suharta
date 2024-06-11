2ccad649f26b2dcb063793a076249a1803d72b22
BryanAustinS <bryanaustin2004@gmail.com>
2024-06-11 15:56:47 +0200

    update the spareparts

--- hci_hda_chiu_suharta/lib/class/fahrrarzt.dart
+++ hci_hda_chiu_suharta/lib/class/fahrrarzt.dart
@@ -1,11 +1,61 @@
 import 'package:hci_hda_chiu_suharta/class/buchung.dart';
 import 'package:hci_hda_chiu_suharta/class/kunde.dart';
-import 'package:hci_hda_chiu_suharta/class/sparepart.dart';
+import 'package:hci_hda_chiu_suharta/class/sparepart.dart'; // Import the Sparepart class
 import 'package:hci_hda_chiu_suharta/class/techniker.dart';
 
-class Fahrrarzt{
+class Fahrrarzt {
   List<Techniker>? alleTechniker;
   List<Buchung>? alleBuchungen;
   List<Kunde>? alleKunden;
-  Map<Sparepart, int>? warehouse; //Fill this with json file
+
+  Map<Sparepart, int>? warehouse = {
+    //Komponente
+    Sparepart(
+      name: 'Brakes',
+      buyPrice: 20, 
+      sellPrice: 50 
+    ): 10,
+    Sparepart(
+      name: 'Chains',
+      buyPrice: 10,
+      sellPrice: 20
+    ): 10,
+    Sparepart(
+      name: 'Saddle',
+      buyPrice: 5,
+      sellPrice: 10
+    ): 10,
+    Sparepart(
+      name: 'Tyres',
+      buyPrice: 7,
+      sellPrice: 15
+    ): 10,
+    Sparepart(
+      name: 'Spokes',
+      buyPrice: 5,
+      sellPrice: 12
+    ): 10,
+
+    //Zubehoer
+    Sparepart(
+      name: 'Bike locks',
+      buyPrice: 3,
+      sellPrice: 10
+    ): 10,
+    Sparepart(
+      name: 'Lights',
+      buyPrice: 2,
+      sellPrice: 7
+    ): 10,
+    Sparepart(
+      name: 'Luggage racks',
+      buyPrice: 12,
+      sellPrice: 20
+    ): 10,
+    Sparepart(
+      name: 'Bike pumps',
+      buyPrice: 3,
+      sellPrice: 10
+    ): 10,
+  };
 }
--- hci_hda_chiu_suharta/lib/class/sparepart.dart
+++ hci_hda_chiu_suharta/lib/class/sparepart.dart
@@ -2,4 +2,10 @@ class Sparepart{
   String? name;
   int? buyPrice; //Price to restock item
   int? sellPrice; //Price to sell to customer
+
+  Sparepart({
+    this.name,
+    this.buyPrice,
+    this.sellPrice,
+  });
 }
